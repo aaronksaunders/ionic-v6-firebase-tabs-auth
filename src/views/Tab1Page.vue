2a7363e0803f1b72d819a93222438c565e51b147
Aaron Saunders <aaronksaunders@gmail.com>
2021-12-19 23:04:30 -0500

    add ionic UI

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,19 +8,31 @@
     <ion-content :fullscreen="true" class="ion-padding">
       <h1>{{ authStore.user?.email }}</h1>
       <pre>{{ JSON.stringify(imageStore?.images, null, 1) }}</pre>
+      <ion-list>
+        <ion-item v-for="image in imageStore?.images" :key="image.uid">
+          <ion-avatar slot="start" style="width: 100px; height: 100px">
+            <img :src="image.url" />
+          </ion-avatar>
+          <ion-label>
+            <h2>{{ image.name }}</h2>
+            <h3>{{ image.imageData.size }} {{ image.imageData.contentType }}</h3>
+            <p>{{ image.imageData?.updated && new Date(image.imageData?.updated)?.toLocaleDateString() }}</p>
+          </ion-label>
+        </ion-item>
+      </ion-list>
       <ion-button @click="doLogout">LOGOUT</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonList, IonLabel } from '@ionic/vue';
 import { useAuthStore, useImageStore } from '@/store';
 import { useRouter } from 'vue-router'
 import { defineComponent, onMounted } from 'vue';
 
 export default defineComponent({
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonItem, IonList, IonLabel },
   name: 'Tab-1',
   setup() {
     const authStore = useAuthStore();
